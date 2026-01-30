8d02f05c8a58fc89635597a8419704fdeece32ca
lauren_m <laurenamolloy@gmail.com>
2026-01-30 10:48:27 +0200

    Initial commit

---
+++ Modules_NPM/app.js
@@ -0,0 +1,6 @@
+const { PI, square } = require('./shelter/math');
+
+const cats = require('./shelter')
+//console.log(PI);
+
+console.log("Require an entire directory!", cats)
---
+++ Modules_NPM/shelter/math.js
@@ -0,0 +1,9 @@
+module.exports.PI = 3.1459;
+
+module.exports.square = x => x * x;
+
+module.exports.add = (x, y) => x + y;
+
+// module.exports.add = add;
+// module.exports.PI = PI;
+// module.exports.square = square;
